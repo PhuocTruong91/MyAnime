3804c770059bdf29759abd5c9ea1bdbb4fe2af98
PhuocTruong91 <phuoctruong554@gmail.com>
2021-10-13 21:05:15 +0700

    truog

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,8 +9,8 @@ import {
     Input,
     Button, WrapForm
 } from './Footer.elements'
-import { FaFacebook, FaInstagram, FaPhoneAlt } from 'react-icons/fa'
-import { GrMail } from 'react-icons/gr'
+// import { FaFacebook, FaInstagram, FaPhoneAlt } from 'react-icons/fa'
+// import { GrMail } from 'react-icons/gr'
 
 const Footer = (e) => {
 
@@ -38,14 +38,14 @@ const Footer = (e) => {
                     <ItemLink to='./anime'>0934802184</ItemLink>
                     <ItemLink to='./anime'>phuoctruong554@gmail.com</ItemLink>
                 </InfoItem>
-                <InfoItem>
+                {/* <InfoItem>
                     <TitleInfoItem>Social Network</TitleInfoItem>
                     <ItemLink to='./anime'>
                         <a target="_blank" href='https://www.facebook.com/P.T.H.9100/'><FaFacebook size={23}></FaFacebook></a>
                         <a target="_blank" href='https://www.instagram.com/phc.trng/'><FaInstagram size={23}></FaInstagram></a>
                         <a target="_blank" href="mailto:someone@yoursite.com"><GrMail size={23}></GrMail></a>
                     </ItemLink>
-                </InfoItem>
+                </InfoItem> */}
             </InfoContainer>
         </FooterContainer>
     );
